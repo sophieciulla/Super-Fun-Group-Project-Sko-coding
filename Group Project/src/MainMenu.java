b4475548d23dc9060ffd34c40dfb213bc194e1c8
Sophie Ciulla <Sophie.Ciulla@MU-C1-09.mullen.pvt.k12.co.us>
2017-12-12 14:19:52 -0700

    modify menu choices

--- Group Project/src/MainMenu.java
+++ Group Project/src/MainMenu.java
@@ -1,11 +1,17 @@
-
+import java.util.Scanner;
 public class MainMenu
 	{
 
 		public static void main(String[] args)
 			{
-				System.out.println("Hello");
-				//hi
+				menuChoice();
+			}	
+			public static void menuChoice()
+			{
+				System.out.println("What would you like to do?");
+				System.out.println("   1) Add or delete a student");
+				System.out.println("   2) Change student grades/schedule");
+				System.out.println("   3) Sort students");
 			}
 
 	}
